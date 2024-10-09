074f55a26f4599893352ff5ddc31f7d34e265d33
Gokamole <gokamole@tutamail.com>
2024-10-09 11:13:58 +0300

    11

--- ACMP3.py
+++ ACMP3.py
@@ -1,16 +1,8 @@
 input_data = open('input.txt', 'r')
 data = input_data.read ()
 a = int(data)
-k = 0
-if a > 0:   
- for i in range (1, a+1):
-  k= k+i
-elif a < 0:
- for i in range (a, 2):
-  k = k+i
-else:
- k=1
+b = (a*a)
 output_data = open ('output.txt', 'w')
-output_data.write(str(k))
+output_data.write(str(b))
 input_data.close()
 output_data.close()
